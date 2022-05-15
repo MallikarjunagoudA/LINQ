6ae5f393ee39d67d4567083a7efc5c19c92c0b3d
MallikarjungoudA <aamallikarjuna18@gmail.com>
2022-05-15 20:23:45 +0530

    linq basic

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -42,6 +42,154 @@ namespace LINQ
                 Console.WriteLine("empname {0}, empmail {1}", e.EmpName, e.EmpMail);
             }
 
+
+
+
+
+
+
+
+
+            //Ienumerable linq demo
+
+            Console.WriteLine("Ienumerable syntax");
+            IEnumerable<Employee> list2 = (from a in emp
+                        where a.EmpName.Contains("gana")
+                        select a).ToList();
+
+            foreach (Employee e in list2)
+            {
+                Console.WriteLine("empname {0}, empmail {1}", e.EmpName, e.EmpMail);
+            }
+
+            //Iquerable linq demo
+
+            IQueryable<Employee> list3   = emp.AsQueryable().Where(e => e.EmpName.Contains("gana"));//.Select(e => e);
+
+            Console.WriteLine("Ienumerable syntax");
+            foreach (Employee e in list3)
+            {
+                Console.WriteLine("empname {0}, empmail {1}", e.EmpName, e.EmpMail);
+            }
+
+
+
+
+
+
+            //chapter 6
+            //linq operators
+            //select in linq
+            /*selct the data with 
+             * other class
+             * ananymous method
+             * with index
+             */
+
+            //basic query 
+
+            var BasicQuery = (from a in emp
+                        select a).ToList();
+
+            Console.WriteLine("Ienumerable syntax");
+            foreach (Employee e in list3)
+            {
+                Console.WriteLine("empname {0}, empmail {1}", e.EmpName, e.Empphone);
+            }
+
+            var BasicMethod= emp.ToList();
+
+            Console.WriteLine("Ienumerable syntax");
+            foreach (Employee e in list3)
+            {
+                Console.WriteLine("empname {0}, empmail {1}", e.EmpName, e.Empphone);
+            }
+
+
+
+
+            /*
+             * operations
+             */
+
+            //fetch only id
+            
+            var Fectchonlyid=(from e in emp
+                              select e.EmpID).ToList();
+
+
+            var FetchOnlyid1 =emp.Select(e => e.EmpID).ToList(); 
+
+
+
+            //fetch only id as string
+
+            var FectchonlyidasString = (from e in emp
+                                select e.EmpID.ToString()).ToList();
+
+            var FetchOnlyid1asString = emp.Select(e => e.EmpID.ToString()).ToList();
+
+
+
+
+
+
+            //fetch data as object
+
+            var FectchonlyidasObj = (from e in emp
+                                     select new Employee() 
+                                     {
+                                         EmpID = e.EmpID,
+                                         EmpName = e.EmpName 
+                                     }
+                                     ).ToList();
+
+
+            var FetchOnlyidasObj = emp.Select(e =>new Employee 
+            {
+                EmpName =e.EmpName,
+                EmpMail=e.EmpMail
+            }).ToList();
+
+
+
+            /* 
+             * fetch data to other class
+             */
+            var FectchToStudent = (from e in emp
+                                     select new Student()
+                                     {
+                                         EmpID = e.EmpID,
+                                         EmpName = e.EmpName
+                                     }
+                                 ).ToList();
+
+
+            var FectchToStudent1 = emp.Select(e => new Student
+            {
+                EmpName = e.EmpName,
+                EmpMail = e.EmpMail
+            }).ToList();
+
+
+
+
+
+
+            /*
+             * select many method
+             */
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 
@@ -54,4 +202,16 @@ namespace LINQ
         public string EmpMail { get; set; }
         
     }
+
+
+    class Student
+    {
+
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public int Empphone { get; set; }
+        public string EmpMail { get; set; }
+
+    }
+
 }
